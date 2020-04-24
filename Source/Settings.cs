e455e977638cf0ba7d30d2a2cd068da3e427afdd
Fluffy <fluffy.l2032@gmail.com>
2020-04-24 15:43:13 +0200

    Release 0.1.53 [nolog]

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -12,20 +12,28 @@ namespace DesirePaths
         public string pathCreateThresholdBuffer;
         public string pathDegradeFactorBuffer;
 
-		public void DoWindowContents(Rect canvas)
-		{
-			var options = new Listing_Standard();
-			options.Begin(canvas);
-            options.TextFieldNumericLabeled( "Path creation threshold (default: 120, lower creates paths faster)", ref pathCreateThreshold, ref pathCreateThresholdBuffer, 50, 1000 );
-            options.TextFieldNumericLabeled( "Path degrade threshold (default: 80, higher removes paths faster)", ref pathDegradeThreshold, ref pathDegradeThresholdBuffer, 0, pathCreateThreshold - 10 );
-            options.TextFieldNumericLabeled( "Path degrade factor (default: .9, lower degrades paths faster)", ref pathDegradeFactor, ref pathDegradeFactorBuffer, .6f, .99f );
-			options.End();
-		}
-		
-		public override void ExposeData()
+        public void DoWindowContents( Rect canvas )
+        {
+            var options = new Listing_Standard();
+            options.Begin( canvas );
+            options.Label( $"Path creation threshold (current: {pathCreateThreshold}, default: 120, lower creates paths faster)" );
+            pathCreateThreshold = (int) options.Slider( pathCreateThreshold, 50, 1000 );
+            options.Gap();
+
+            options.Label( $"Path degrade threshold (current: {pathDegradeThreshold}, default: 80, higher removes paths faster)" );
+            pathDegradeThreshold = (int) options.Slider( pathDegradeThreshold, 0, pathCreateThreshold - 10 );
+            options.Gap();
+
+            options.Label( $"Path degrade factor (current: {pathDegradeFactor}, default: .9, lower degrades paths faster)" );
+            pathDegradeFactor = options.Slider( pathDegradeFactor, .5f, .99f );
+            options.End();
+        }
+
+        public override void ExposeData()
         {
             Scribe_Values.Look( ref pathCreateThreshold, "pathCreateThreshold", 120 );
             Scribe_Values.Look( ref pathDegradeThreshold, "pathDegradeThreshold", 80 );
+            Scribe_Values.Look( ref pathDegradeFactor, "pathDegradeFactor", .9f );
         }
 	}
 }
--- Source/description.md
+++ Source/description.md
@@ -4,16 +4,16 @@ Creates packed dirt paths when colonists frequently take the same path.
 
 Packed dirt has a higher move speed than most other 'natural' terrains, but it does track a lot of dirt around and has no fertility. If your colonists frequently walk through your farms, they will eventually trample the fields.
 
-Natural paths will form when colonists frequently take the same path, and will eventually degrade back into the original terrain if not walked on.
-
-In the real world, such paths are called [desire paths](https://en.wikipedia.org/wiki/Desire_path). They exist everywhere, and you probably never paid them much attention.
+Packed dirt paths will form when colonists frequently take the same path over grass, soil, dirt and other natural terrain. The packed dirt paths will eventually degrade back into the original terrain if not walked on. In the real world, such paths are called [desire paths](https://en.wikipedia.org/wiki/Desire_path), Elephant paths, pirate paths, or just simply shortcuts.
 
 ![Desire Path](https://i.ibb.co/gF6Sw3t/image.png)
 
 # Recommended
 
-I recommend using this together with
+If you find that your colonists keep walking through your fields, I recommend using this together with
 [Path Avoid](https://steamcommunity.com/sharedfiles/filedetails/?id=1180719857), so that you can better steer where your colonists may and may not walk.
+
+That said, if such desire paths form where you don't want them, it's really a sign that you need to change your base design!
 
 # Known Issues
 
