9e50cf6b0bec2acdd27db1a1cb51dc1b3c517887
Fluffy <fluffy.l2032@gmail.com>
2020-05-01 22:05:32 +0200

    tweak snow clearance rate, add stone smoothing

--- Source/DesireGrid.cs
+++ Source/DesireGrid.cs
@@ -11,19 +11,25 @@ namespace DesirePaths
 {
     public class DesireGrid : MapComponent
     {
-        private float[]      walkGrid;
+        private float[] walkGrid;
+        private float[] stoneGrid;
         private TerrainDef[] originalTerrain;
 
-        public DesireGrid( Map map ) : base( map )
+        public DesireGrid(Map map) : base(map)
         {
             var n = map.cellIndices.NumGridCells;
             walkGrid = new float[n];
-            for ( var i = 0; i < n; i++ ) walkGrid[i] = 0;
+            stoneGrid = new float[n];
+            for (var i = 0; i < n; i++)
+            {
+                walkGrid[i] = 0;
+                stoneGrid[i] = 0;
+            }
 
             originalTerrain = new TerrainDef[n];
         }
 
-        public bool IsPackable( TerrainDef terrain )
+        public bool IsPackable(TerrainDef terrain)
         {
             return terrain.takeFootprints;
         }
@@ -31,74 +37,87 @@ namespace DesirePaths
         public override void MapComponentTick()
         {
             base.MapComponentTick();
-            if ( GenTicks.TicksGame % 10                          == 0 ) DoPheromoneTick();
-            if ( GenTicks.TicksGame % ( GenDate.TicksPerDay / 12 ) == 0 ) DoUpdateTick();
+            if (GenTicks.TicksGame % 20 == 0) DoWalkGridTick();
+            if (GenTicks.TicksGame % (GenDate.TicksPerDay / 12) == 0) DoUpdateTick();
         }
 
-        public void DoPheromoneTick()
+        public void DoWalkGridTick()
         {
-            foreach ( var pawn in map.mapPawns.AllPawnsSpawned.Where( p => !p.Dead && !p.Downed && p.Awake() ) )
+            foreach (var pawn in map.mapPawns.AllPawnsSpawned.Where(p => !p.Dead && !p.Downed && p.Awake()))
             {
                 var weight = 1f;
-                if ( pawn.RaceProps?.Animal ?? false )
+                if (pawn.RaceProps?.Animal ?? false)
                     weight *= 1 / 4f;
                 weight *= pawn.BodySize;
-                if ( !pawn.pather.MovingNow )
+                if (!pawn.pather.MovingNow)
                     weight *= 1 / 10f;
-                walkGrid[map.cellIndices.CellToIndex( pawn.PositionHeld )] += weight;
+                var index = map.cellIndices.CellToIndex(pawn.Position);
+                walkGrid[index] += weight;
+                stoneGrid[index] += weight;
+
+                // also remove snow where we're walking, directly on the snowgrid
+                map.snowGrid.AddDepth(pawn.Position, -.02f * DesirePaths.Settings.snowClearFactor * pawn.BodySize);
+
 #if DEBUG
-                map.debugDrawer.FlashCell( pawn.Position, weight );
+                map.debugDrawer.FlashCell(pawn.Position, weight);
 #endif
-
-                // also remove snow where we're walking
-                map.snowGrid.AddDepth( pawn.Position, -.02f );
-
             }
         }
 
         public void DoUpdateTick()
         {
-            for ( var i = 0; i < walkGrid.Length; i++ )
+            for (var i = 0; i < map.cellIndices.NumGridCells; i++)
             {
-                if ( walkGrid[i] > DesirePaths.Settings.pathCreateThreshold )
-                    TryCreatePath( i );
-                walkGrid[i] *= Mathf.Sqrt( DesirePaths.Settings.pathDegradeFactor );
-                if ( walkGrid[i] < DesirePaths.Settings.pathDegradeThreshold )
-                    TryRemovePath( i );
+                if (walkGrid[i] > DesirePaths.Settings.pathCreateThreshold)
+                    TryCreatePath(i);
+                walkGrid[i] *= DesirePaths.Settings.pathDegradeFactor;
+                if (walkGrid[i] < DesirePaths.Settings.pathDegradeThreshold)
+                    TryRemovePath(i);
+
+                if (stoneGrid[i] > DesirePaths.Settings.stoneSmoothThreshold)
+                    TrySmooth(i);
             }
         }
 
-        public void TryCreatePath( int index )
+        public void TryCreatePath(int index)
         {
-            var cell    = map.cellIndices.IndexToCell( index );
-            var terrain = cell.GetTerrain( map );
-            if ( terrain == TerrainDefOf.PackedDirt || !IsPackable( terrain ) )
+            var cell = map.cellIndices.IndexToCell(index);
+            var terrain = cell.GetTerrain(map);
+            if (terrain == TerrainDefOf.PackedDirt || !IsPackable(terrain))
                 return;
 
             originalTerrain[index] = terrain;
-            map.terrainGrid.SetTerrain( cell, TerrainDefOf.PackedDirt );
+            map.terrainGrid.SetTerrain(cell, TerrainDefOf.PackedDirt);
         }
 
-        public void TryRemovePath( int index )
+        public void TryRemovePath(int index)
         {
-            if ( originalTerrain[index] == null )
+            if (originalTerrain[index] == null)
                 return;
 
-            var cell    = map.cellIndices.IndexToCell( index );
-            var terrain = cell.GetTerrain( map );
-            if ( terrain != TerrainDefOf.PackedDirt )
+            var cell = map.cellIndices.IndexToCell(index);
+            var terrain = cell.GetTerrain(map);
+            if (terrain != TerrainDefOf.PackedDirt)
                 return;
 
-            map.terrainGrid.SetTerrain( cell, originalTerrain[index] );
+            map.terrainGrid.SetTerrain(cell, originalTerrain[index]);
             originalTerrain[index] = null;
+        }
+
+        public void TrySmooth(int index)
+        {
+            var cell = map.cellIndices.IndexToCell(index);
+            var terrain = cell.GetTerrain(map);
+            if (terrain.affordances.Contains(TerrainAffordanceDefOf.SmoothableStone))
+                map.terrainGrid.SetTerrain(cell, terrain.smoothedTerrain);
         }
 
         public void DebugDraw()
         {
             Find.TickManager.CurTimeSpeed = TimeSpeed.Paused;
-            Log.Message( $"walk: min: {walkGrid.Min()}, max: {walkGrid.Max()}, avg: {walkGrid.Average()}" );
-            foreach ( var cell in map.AllCells )
-                map.debugDrawer.FlashCell( cell, walkGrid[map.cellIndices.CellToIndex( cell )] );
+            Log.Message($"walk: min: {walkGrid.Min()}, max: {walkGrid.Max()}, avg: {walkGrid.Average()}");
+            foreach (var cell in map.AllCells)
+                map.debugDrawer.FlashCell(cell, walkGrid[map.cellIndices.CellToIndex(cell)]);
         }
 
 #if DEBUG
@@ -107,41 +126,41 @@ namespace DesirePaths
             base.MapComponentOnGUI();
 
             var pos = UI.MouseCell();
-            Widgets.Label( new Rect( 10, Screen.height * 1 / 3f, 300, 300 ),
-                           $"x: {pos.x}, y: {pos.z}\n"                               +
-                           $"walk: {walkGrid[map.cellIndices.CellToIndex( pos )]}\n" +
-                           $"pathCost: {pos.GetTerrain( map ).pathCost}\n" );
+            Widgets.Label(new Rect(10, Screen.height * 1 / 3f, 300, 300),
+                           $"x: {pos.x}, y: {pos.z}\n" +
+                           $"walk: {walkGrid[map.cellIndices.CellToIndex(pos)]}\n" +
+                           $"pathCost: {pos.GetTerrain(map).pathCost}\n");
 
-            if ( KeyBindingDefOf.Accept.KeyDownEvent )
+            if (KeyBindingDefOf.Accept.KeyDownEvent)
                 DebugDraw();
         }
 #endif
 
         private static Dictionary<ushort, TerrainDef> terrainsByHash =
-            DefDatabase<TerrainDef>.AllDefsListForReading.ToDictionary( d => d.shortHash, d => d );
+            DefDatabase<TerrainDef>.AllDefsListForReading.ToDictionary(d => d.shortHash, d => d);
         public override void ExposeData()
         {
             base.ExposeData();
 
-            if ( Scribe.mode == LoadSaveMode.LoadingVars )
+            if (Scribe.mode == LoadSaveMode.LoadingVars)
             {
-                walkGrid        = new float[map.cellIndices.NumGridCells];
+                walkGrid = new float[map.cellIndices.NumGridCells];
                 originalTerrain = new TerrainDef[map.cellIndices.NumGridCells];
             }
 
             MapExposeUtility.ExposeUshort(
-                map, ( cell ) => (ushort) ( walkGrid[map.cellIndices.CellToIndex( cell )] * 100 ),
-                ( cell, val ) => walkGrid[map.cellIndices.CellToIndex( cell )] = val / 100f,
-                "walkGrid" );
+                map, (cell) => (ushort)(walkGrid[map.cellIndices.CellToIndex(cell)] * 100),
+                (cell, val) => walkGrid[map.cellIndices.CellToIndex(cell)] = val / 100f,
+                "walkGrid");
 
             MapExposeUtility.ExposeUshort(
-                map, ( cell ) => originalTerrain[map.cellIndices.CellToIndex( cell )]?.shortHash ?? 0,
-                ( cell, val ) =>
+                map, (cell) => originalTerrain[map.cellIndices.CellToIndex(cell)]?.shortHash ?? 0,
+                (cell, val) =>
                 {
-                    if ( val != 0 && terrainsByHash.TryGetValue( val, out TerrainDef terrain ) )
-                        originalTerrain[map.cellIndices.CellToIndex( cell )] = terrain;
+                    if (val != 0 && terrainsByHash.TryGetValue(val, out TerrainDef terrain))
+                        originalTerrain[map.cellIndices.CellToIndex(cell)] = terrain;
 
-                }, "originalTerrain" );
+                }, "originalTerrain");
         }
     }
 }
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -3,34 +3,46 @@ using Verse;
 
 namespace DesirePaths
 {
-	public class Settings : ModSettings
-	{
-        public int pathDegradeThreshold = 120;
-        public int pathCreateThreshold = 80;
+    public class Settings : ModSettings
+    {
+        public int pathCreateThreshold = 120;
+        public int pathDegradeThreshold = 80;
         public float pathDegradeFactor = .9f;
+        public float snowClearFactor = .5f;
+        public int stoneSmoothThreshold = 1000;
 
-        public void DoWindowContents( Rect canvas )
+        public void DoWindowContents(Rect canvas)
         {
             var options = new Listing_Standard();
-            options.Begin( canvas );
-            options.Label( $"Path creation threshold (current: {pathCreateThreshold}, default: 120, lower creates paths faster)" );
-            pathCreateThreshold = (int) options.Slider( pathCreateThreshold, 50, 1000 );
+            options.Begin(canvas);
+            options.Label($"Path creation threshold (current: {pathCreateThreshold}, default: 120, lower creates paths faster)");
+            pathCreateThreshold = (int)options.Slider(pathCreateThreshold, 50, 1000);
             options.Gap();
 
-            options.Label( $"Path degrade threshold (current: {pathDegradeThreshold}, default: 80, higher removes paths faster)" );
-            pathDegradeThreshold = (int) options.Slider( pathDegradeThreshold, 0, pathCreateThreshold - 10 );
+            options.Label($"Path degrade threshold (current: {pathDegradeThreshold}, default: 80, higher removes paths faster)");
+            pathDegradeThreshold = (int)options.Slider(pathDegradeThreshold, 0, pathCreateThreshold - 10);
             options.Gap();
 
-            options.Label( $"Path degrade factor (current: {pathDegradeFactor}, default: .9, lower degrades paths faster)" );
-            pathDegradeFactor = options.Slider( pathDegradeFactor, .5f, .99f );
+            options.Label($"Path degrade factor (current: {pathDegradeFactor:N2}, default: .9, lower degrades paths faster)");
+            pathDegradeFactor = options.Slider(pathDegradeFactor, .5f, .99f);
+            options.Gap();
+
+            options.Label($"Snow path creation speed (current: {snowClearFactor:P}, default: 50%, higher removes snow quicker)");
+            snowClearFactor = options.Slider(snowClearFactor, 0f, 1f);
+            options.Gap();
+
+            options.Label($"How long does it take for rough stone to become smoothed with use? (current: {stoneSmoothThreshold}, default: 1000, higher takes longer)");
+            stoneSmoothThreshold = (int)options.Slider(stoneSmoothThreshold, 100, 10000);
             options.End();
         }
 
         public override void ExposeData()
         {
-            Scribe_Values.Look( ref pathCreateThreshold, "pathCreateThreshold", 120 );
-            Scribe_Values.Look( ref pathDegradeThreshold, "pathDegradeThreshold", 80 );
-            Scribe_Values.Look( ref pathDegradeFactor, "pathDegradeFactor", .9f );
+            Scribe_Values.Look(ref pathCreateThreshold, "pathCreateThreshold", 120);
+            Scribe_Values.Look(ref pathDegradeThreshold, "pathDegradeThreshold", 80);
+            Scribe_Values.Look(ref pathDegradeFactor, "pathDegradeFactor", .9f);
+            Scribe_Values.Look(ref snowClearFactor, "snowClearFactor", .5f);
+            Scribe_Values.Look(ref stoneSmoothThreshold, "stoneSmoothThreshold", 1000);
         }
-	}
+    }
 }
